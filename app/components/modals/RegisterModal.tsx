cdfe865f4ba09629038527d637cb6ee74d96bb1e
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-12 17:50:42 +0530

    changes

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,4 +1,17 @@
-import React from "react";
+'use client';
+
+import React, {useState, useCallback} from "react";
+import axios from 'axios';
+import { AiFillGithub } from "react-icons/ai";
+import {FcGoogle} from 'react-icons/fc'
+import {
+    FieldValues,
+    SubmitHandler,
+    useForm
+} from 'react-hook-form'
+
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
 
 const RegisterModal = () => {
 
