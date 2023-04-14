9089aff1279116b5251ea4fc47ef36642f8a263c
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-14 12:30:25 +0530

    changes

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -104,6 +104,36 @@ label="Continue with Github"
 icon={AiFillGithub}
 onClick={() => {}}
 />
+<div 
+className="
+text-neutral-500
+text-center
+mt-4
+font-light
+"
+>
+<div className="
+flex
+flex-row
+justify-center
+gap-2
+">
+    <div> 
+    Already have an account? 
+    </div>
+  
+
+<div className="
+text-neutral-800
+cursor-pointer
+hover:underline
+"
+onClick={registerModal.onClose}
+>
+    Log in
+</div>
+</div>
+</div>
 
     </div>
 )
