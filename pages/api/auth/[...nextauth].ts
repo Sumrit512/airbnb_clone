110007ad13f23712bc947440063ca2a2c2de5fae
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-14 15:15:58 +0530

    changes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { AuthOptions } from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import  CredentialsProvider  from "next-auth/providers";
 import prisma from "@/app/libs/prismadb";
+import bcrypt from "bcrypt"
+
 
 
 export const authOptions: AuthOptions = {
@@ -16,5 +19,47 @@ export const authOptions: AuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
-    ]
-}
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                email: { label: 'email', type:'text'},
+                password: {label: 'password', type: 'password'}
+            },
+            async authorize(credentials: { email: any; password: any; }) {
+             if(!credentials?.email || !credentials?.password) {
+                throw new Error('Invalid credentials');
+             } 
+
+              const user = await prisma.user.findUnique({
+                where: {
+                    email: credentials.email
+                }
+              });
+        if(!user || !user?.hashedPassword) {
+            throw new Error('Invalid Password');
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+            credentials.password,
+            user.hashedPassword
+        );
+
+        if(!isCorrectPassword) {
+            throw new Error('Invalid credentials');
+        }
+               
+        return user;
+           }
+        })
+    ],
+    pages: {
+     signIn: '/'
+    },
+    debug: process.env.NODE_ENV ==='development',
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions)
