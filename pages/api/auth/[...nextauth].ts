69c5245ba74e60c75a73366dbe2ddd937de1a31e
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-14 13:57:40 +0530

    changes

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import prisma from "@/app/libs/prismadb";
+
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+    ]
+}
