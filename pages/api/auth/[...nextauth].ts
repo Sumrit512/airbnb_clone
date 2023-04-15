702bf503afe727f1232cd99c60a0c0a8061c588b
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-15 18:22:55 +0530

    changes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -36,7 +36,7 @@ export const authOptions: AuthOptions = {
                 }
               });
         if(!user || !user?.hashedPassword) {
-            throw new Error('Invalid');
+            throw new Error('Invalid Password');
         }
 
         const isCorrectPassword = await bcrypt.compare(
