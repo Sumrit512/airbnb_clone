03b1abf52261d50b0b95ac0a385d10a02f60e440
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-15 18:22:04 +0530

    chnages

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -36,7 +36,7 @@ export const authOptions: AuthOptions = {
                 }
               });
         if(!user || !user?.hashedPassword) {
-            throw new Error('Invalid Password');
+            throw new Error('Invalid');
         }
 
         const isCorrectPassword = await bcrypt.compare(
