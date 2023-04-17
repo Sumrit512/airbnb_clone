580a08c33e7dcb4b2af5b28081b0ea3cb70b5e55
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-17 12:31:14 +0530

    changes

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -5,6 +5,7 @@ import {TbBeach} from 'react-icons/tb'
 import {GiWindmill} from 'react-icons/gi'
 import {MdOutlineVilla} from 'react-icons/md'
 import CategoryBox from "../CategoryBox";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export const categories = [
     {
@@ -26,6 +27,15 @@ export const categories = [
 
 const Categories = () => {
 
+const params = useSearchParams()
+const category = params?.get('category')
+const pathname = usePathname()
+
+const isMainPage = pathname ==='/';
+
+if(!isMainPage){
+    return null;
+}
 
     return (
         <Container >
