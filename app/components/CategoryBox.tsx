40b5c7ee5be612155c0a416f858a4a346517eee0
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-17 12:27:09 +0530

    changes

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,22 +1,54 @@
 'use client';
 
-import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from 'query-string'
 
 
 interface CategoryBoxProps{
     label : string;
-    icon: IconType | undefined;
+    icon: IconType;
     selected?: boolean;
 }
 
 const CategoryBox: React.FC<CategoryBoxProps>= ({
     label,
-    icon,
+    icon: Icon,
 selected
 }) => {
+    const router = useRouter();
+    const params = useSearchParams();
+
+    const handleClick = useCallback(()=> {
+   let currentQuery = {} ;
+
+   if(params) {
+    currentQuery = qs.parse(params.toString());
+   }
+
+   const updatedQuery: any = {
+    ...currentQuery,
+    category: label,
+   }
+
+   if(params?.get('category') === label) {
+    delete updatedQuery.category;
+   }
+
+   const url = qs.stringifyUrl({
+    url: '/',
+    query: updatedQuery
+   }, {skipNull: true})
+ router.push(url)    
+
+},[label, params, router])
+
+
 return(
-    <div className={
+    <div 
+    onClick={handleClick}
+    className={
         `
         flex
         flex-col
@@ -33,7 +65,13 @@ return(
         `
     }>
 
-
+ <Icon size={26}/>
+ <div className="
+ font-medium
+ text-sm
+ ">
+    
+ </div>
     </div>
 )
 
