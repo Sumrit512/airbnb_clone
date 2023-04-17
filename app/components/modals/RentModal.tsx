45248f4b0809ef62878934378c2a42e8be62e99c
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-17 15:41:09 +0530

    changes

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,7 +1,8 @@
 "use client";
-import {useState} from 'react'
+import {useMemo, useState} from 'react'
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
+import Heading from '../Heading';
 
 enum STEPS {
     CATEGORY = 0,
@@ -25,15 +26,44 @@ const onNext = () => {
     setStep((value) => value + 1)
 }
 
+const actionLabel = useMemo(() => {
+if( step === STEPS.PRICE) {
+    return 'Create';
+}
+
+return 'Next';
+
+}, [step])
+
+const secondaryActionLabel = useMemo(() => {
+    if(step === STEPS.CATEGORY) {
+        return undefined;
+    }
+    return 'Back';
+
+}, [step])
+
+let bodyContent = (
+    <div>
+        <Heading 
+        title='which of these best describes your place?'
+        subtitle='Pick a category'
+        />
+    </div>
+)
+
     const rentModal = useRentModal()
     return (
     
-    <Modal
+    <Modal 
     isOpen={rentModal.isOpen}
     onClose={rentModal.onClose}
     onSubmit={rentModal.onClose}
-    actionLabel="Submit"
+    actionLabel={actionLabel} 
+    secondaryActionLabel={secondaryActionLabel}
+    secondaryAction={stepp === STEPS.CATEGORY? undefined : onBack }
     title="Airbnb your home"
+    body={bodyContent}
     />
     )
 }
