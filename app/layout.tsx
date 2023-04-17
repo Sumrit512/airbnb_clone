f9fe912ac65c69cdd0c7ad69faae83fd6858ec22
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-17 15:21:09 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
 
 export const metadata = {
   title: 'Airbnb',
@@ -32,6 +33,8 @@ export default async function RootLayout({
        
         <ClientOnly>
      <LoginModal />
+     <RegisterModal />
+     <RentModal />
         <Navbar currentUser={currentUser} />
         <ToasterProvider/>
         </ClientOnly> 
