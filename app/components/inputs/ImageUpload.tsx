198990d5de4ab7bc4a42de145b2acfd67ec2267f
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-18 13:38:18 +0530

    chnages

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -1,11 +1,70 @@
 'use client';
 
-const ImageUpload = () => {
+import { CldUploadButton, CldUploadWidget} from "next-cloudinary";
+import Image from "next/image";
+import React, { useCallback } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
+
+declare global {
+    var cloudinary: any;
+}
+
+interface ImageUploadProps {
+    onChange: (value: string) => void;
+    value: string;
+}
+
+
+const ImageUpload: React.FC<ImageUploadProps>= ({
+    onChange,
+    value
+}) => {
+
+const handleUpload = useCallback((result: any) => {
+    onChange(result.info.secure_url)
+},[onChange])
 
     return (
-        <div>
-            
-        </div>
+<CldUploadWidget
+onUpload={handleUpload}
+uploadPreset=""
+options={{
+    maxFiles: 1
+}}
+>
+    {({  open }) => {
+        return (
+          <div onClick={() => open?.()}
+          className="s
+          relative
+          cursor-pointer
+          hover:opacity-70
+          transition
+          border-dashed
+          border-2
+          p-20
+          border-neutral-300
+          flex
+          flex-col
+          justify-center
+          items-center
+          gap-4
+          text-neutral-600
+          "
+          >
+          <TbPhotoPlus size={50}/>
+            <div className="
+            font-semibold
+            text-lg
+            ">
+          Click to upload
+            </div>
+            </div>
+        )
+    }}
+
+
+</CldUploadWidget>
     )
 }
 
