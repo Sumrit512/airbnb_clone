f773a22f562d8115702cd2de7802eccaa2a35de0
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-18 12:04:01 +0530

    changes

--- app/components/inputs/Counter.tsx
+++ app/components/inputs/Counter.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Value } from "@prisma/client/runtime";
 import React, { useCallback } from "react";
-import { AiOutlineMinus } from "react-icons/ai";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 interface CounterProps {
     title: string;
@@ -81,7 +81,31 @@ return(
                 >
                      <AiOutlineMinus/>
                 </div>
-
+                <div className="
+                font-light
+                text-xl 
+                text-neutral-600
+                ">
+                 {value}
+                </div>
+                <div
+                onClick={onAdd}
+                className="
+                w-10
+                h-10
+                rounded-full
+                border-[1px]
+                border-neutral-400
+                flex
+                items-center
+                justify-center
+                text-neutral-600
+                cursor-pointer
+                hover:opacity-80
+                transition
+                ">
+              <AiOutlinePlus />
+                </div>
             </div>
                 
         </div>
