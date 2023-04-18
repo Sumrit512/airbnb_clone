fc827e108ce2db4540de4333396d2788d12f38bc
RJSYS Soft Solution <sumritarora.512@gmail.com>
2023-04-18 16:32:30 +0530

    changes

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -10,5 +10,41 @@ import prisma from '@/app/libs/prismadb'
     return NextResponse.error()
   } 
 
+  const body = await request.json()
+
+  const {
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location, 
+    price
+  } = body;
+
+  Object.keys(body).forEach((value:any) => {
+    if(!body[value]) {
+        NextResponse.error()
+    }
+  })
+
+  const listing = await prisma.listing.create({
+    data: {
+        title,
+        description,
+        imageSrc,
+        category,
+        roomCount,
+        bathroomCount,
+        guestCount,
+        locationValue: location.value,
+        price: parseInt(price, 10),
+        userId: currentUser.id
+    }
+  })
+
+  return NextResponse.json(listing)
 
 }
